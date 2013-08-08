dfac0b80c9064f89b08c738c013e72ecaf00f3e7
pedropolis <pete@pjwphoto.net>
2013-08-08 01:04:19 +0100

    tidy reader.sh

--- reader.sh
+++ reader.sh
@@ -5,12 +5,15 @@
 target=$1
 hostnames=`redis-cli -h server3 smembers hostnames`
 
+function info() {
+echo 'BASHREDMOND pretty raw version #1'
+}
 
 function show_target() {
 echo '*************************************************************'
 echo `date`
 echo looking up $target ...
-echo '**********'
+echo '******'
 echo 'Uptime is          :       '`redis-cli -h server3 get "$target:uptime"`
 echo 'Avg. Load          :       '`redis-cli -h server3 get "$target:load"`
 echo 'CPU temp           :       '`redis-cli -h server3 get "$target:temp"`
@@ -21,19 +24,20 @@ echo '*************************************************************'
 }
 
 function overview() {
-
+echo Database holds these hostnames: $hostnames
+echo '******'
 for host in $hostnames
 do
 echo $host
 echo 'Uptime:  '`redis-cli -h server3 get "$host:uptime"`
 echo 'Load  :  '`redis-cli -h server3 get "$host:load"`
 echo 'Temp  :  '`redis-cli -h server3 get "$host:temp"`
+echo '******'
 done
-
 
 }
 
-
+info
 if [ "$target" = "" ]
 	then 
 		overview

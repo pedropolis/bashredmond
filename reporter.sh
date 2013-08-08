0ddc0021d4a7e1f46bbfcc61892b92bba4b17f08
pedropolis <pete@pjwphoto.net>
2013-08-09 00:38:51 +0100

    fix minerd check and add comments and new data

--- reporter.sh
+++ reporter.sh
@@ -3,6 +3,7 @@
 
 host=`hostname`
 
+#test if our hostname is in the db list, add it if not.
 domlist=`redis-cli -h server3 sismember hostnames $host`
 
 if [ $domlist = 0 ]
@@ -10,6 +11,7 @@ if [ $domlist = 0 ]
 	redis-cli -h server3 sadd hostnames $host
 fi
 
+#check if we are running Music Player Daemon
 ps -C mpd
 mpd=$?
 
@@ -20,20 +22,28 @@ if [ $mpd = 0 ]
 	redis-cli -h server3 set "`hostname`:mpd" no
 fi
 
+#check if cpuminer is running and grab process info
 ps -C minerd
 minerd=$?
 
-if [ $mpd = 0 ]
+if [ $minerd = 0 ]
 	then
 	redis-cli -h server3 set "`hostname`:minerd" yes
-	
+	redis-cli -h server3 set "`hostname`:minerinfo" `ps `ps -C minerd|cut -b 1-5|grep ^....[0-9]`|grep ^....[0-9]|cut -d' ' -f 1,12,17`
 	else
 	redis-cli -h server3 set "`hostname`:minerd" no
+	redis-cli -h server3 set "`hostname`:minerinfo" 
+
 fi
+#fetch a bunch of other info we want
+
+redis-cli -h server3 set "`hostname`:uptime" "`uptime|cut -d "," -f 1,2|cut -d' ' -f 4-`"
+redis-cli -h server3 set "`hostname`:load" "`uptime|cut -d',' -f 4-|cut -b 17-`"
+
+redis-cli -h server3 set "`hostname`:temp" "`acpi -t|cut -d' ' -f 4`"
+
+redis-cli -h server3 set "`hostname`:gping" "`ping -c 2 google.com|cut -sd '/' -f 5`"
 
 
 redis-cli -h server3 set "`hostname`:time" "`date +%D-%H:%M:%S`"
-redis-cli -h server3 set "`hostname`:uptime" "`uptime|cut -d "," -f 1,2|cut -d' ' -f 4-`"
-redis-cli -h server3 set "`hostname`:load" "`uptime|cut -d',' -f 4-|cut -b 17-`"
-redis-cli -h server3 set "`hostname`:temp" "`acpi -t|cut -d' ' -f 4`"
-redis-cli -h server3 set "`hostname`:gping" "`ping -c 2 google.com|cut -sd '/' -f 5`"
+
